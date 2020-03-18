ace3938dd96c6e18a70bc6507752daf8b1fb41f4
Osvaldo Garcia <aoga88@gmail.com>
2020-03-18 04:00:16 -0600

    footer partner

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                2019 - {{date('Y')}} &copy; Desarrollado por <a href="https://kadoo.mx">Kadoo.mx</a>
+                2019 - {{date('Y')}} &copy; Desarrollado por @if(empty(env('PARTNER'))) <a href="https://kadoo.mx">Kadoo.mx</a> @else {{ env('PARTNER') }} @endif
             </div>
         </div>
     </div>
