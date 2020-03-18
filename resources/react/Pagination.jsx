2cda0cdf317efd5ec17ccd84cafe882335889a09
Osvaldo Garcia <aoga88@gmail.com>
2020-03-18 01:55:38 -0600

    pages valdiation¡

--- resources/react/Pagination.jsx
+++ resources/react/Pagination.jsx
@@ -19,11 +19,11 @@ export default class Pagination extends Component {
         return <div {...this.props.domProps}>
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
-                    <li className={this.props.page == 1?"page-item disabled":"page-item"}>
+                    {this.props.pages.length != 0 ? <li className={this.props.page == 1?"page-item disabled":"page-item"}>
                         <a className={this.props.loading?"page-link disabled":"page-link"} href="javascript:;" onClick={this.prev.bind(this)}>
                             Anterior
                         </a>
-                    </li>
+                    </li>:null}
                     {this.props.pages.map((item, key) => {
                         return <li key={key} className={item == this.props.page?"page-item active":"page-item"}>
                             <a className={this.props.loading?"page-link disabled":"page-link"} href="javascript:;" onClick={this.props.setPage.bind(this, item)}>
@@ -31,11 +31,11 @@ export default class Pagination extends Component {
                             </a>
                         </li>
                     })}
-                    <li className={this.props.page == this.props.pages.length?"page-item disabled":"page-item"}>
+                    {this.props.pages.length != 0 ? <li className={this.props.page == this.props.pages.length?"page-item disabled":"page-item"}>
                         <a className={this.props.loading?"page-link disabled":"page-link"} href="javascript:;" onClick={this.next.bind(this)}>
                             Siguiente
                         </a>
-                    </li>
+                    </li>:null}
                 </ul>
             </nav>
         </div>
